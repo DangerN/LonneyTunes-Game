ea7406fe2282a38cbbfffa9935701a3cf5d86e71
Crystal Danger <cdneal091@gmail.com>
2020-02-08 16:51:51 -0500

    stuff we did

--- src/App.js
+++ src/App.js
@@ -8,9 +8,24 @@ import looney from  "./looney.json"
 class App extends Component{
     state={
         looney,
+        looneyPieces: [],
         highscore:0,
         score:0,
         answer:[]
+    }
+    componentDidMount(){
+      let newPieces = []
+      looney.forEach((loon) => {
+        let newLoon = loon
+        newLoon.id = `${newLoon.id}a`
+        newPieces.push(newLoon)
+        newLoon.id[1] = 'b'
+        newPieces.push(newLoon)
+      })
+      console.log(newPieces);
+    }
+    clicked(id){
+
     }
     render(){
         return(
@@ -23,13 +38,18 @@ class App extends Component{
             {this.state.looney.map(character=>{
                 return(
                 <Card
+                {...character}
+
                 id={character.id}
-                key={character.id}
                 image={character.image}
+
+
+                key={character.id}
+                clicked={this.clicked}
                 />
                 )
             })}
-        
+
         </Wrapper>
     </div>
         )
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,20 +1,46 @@
-import React from "react"
+import React, {useState} from "react"
 import "./style.css"
 
 
 
 function Card(props){
+  const [showing, setShowing] = useState(false)
+
+  const handleClick = () => {
+    setShowing(!showing)
+    props.clicked(props.id)
+  }
+
+  const cardFront = () => {
+    return (
+      <div className="row row-cols-1 row-cols-md-3">
+          <div className="col m-1">
+              <div>
+              <div className="card">
+                  <img src={props.image} className="card-img-top" alt={props.id} onClick={handleClick}></img>
+              </div>
+          </div>
+      </div>
+  </div>
+    )
+  }
+
+  const cardBack = () => {
+    return (
+      <div className="row row-cols-1 row-cols-md-3">
+          <div className="col m-1">
+              <div>
+              <div className="card" onClick={handleClick}>
+              </div>
+          </div>
+      </div>
+  </div>
+    )
+  }
+
     return(
         <div>
-            <div className="row row-cols-1 row-cols-md-3">
-                <div className="col m-1">
-                    <div>
-                    <div className="card">
-                        <img src={props.image} className="card-img-top" alt={props.id} onClick={()=>props.clicked(props.id)}></img>
-                    </div>
-                </div>
-            </div>
-        </div>
+          {showing ? cardFront() : cardBack()}
         </div>
     )
 }
