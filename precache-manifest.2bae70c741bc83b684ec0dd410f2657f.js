4b7417152eab56363ac21f5a658d8165ed4b083e
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-02-07 23:10:56 -0800

    Updates

---
+++ precache-manifest.2bae70c741bc83b684ec0dd410f2657f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72aac878320835f9b5bf6e3ec8a09a5d",
+    "url": "/looneytunes/images/e/e5/Gunfade.gif/revision/latest/scale-to-width-down/250/index.html"
+  },
+  {
+    "revision": "b05d33187b4cd6a7615b",
+    "url": "/looneytunes/images/e/e5/Gunfade.gif/revision/latest/scale-to-width-down/250/static/css/main.f05a0c94.chunk.css"
+  },
+  {
+    "revision": "e15d8a62eb8ccf113183",
+    "url": "/looneytunes/images/e/e5/Gunfade.gif/revision/latest/scale-to-width-down/250/static/js/2.b43f8c0c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/looneytunes/images/e/e5/Gunfade.gif/revision/latest/scale-to-width-down/250/static/js/2.b43f8c0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b05d33187b4cd6a7615b",
+    "url": "/looneytunes/images/e/e5/Gunfade.gif/revision/latest/scale-to-width-down/250/static/js/main.cd7dae6e.chunk.js"
+  },
+  {
+    "revision": "e03a4b03340f1e2d7080",
+    "url": "/looneytunes/images/e/e5/Gunfade.gif/revision/latest/scale-to-width-down/250/static/js/runtime-main.42c22f70.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/looneytunes/images/e/e5/Gunfade.gif/revision/latest/scale-to-width-down/250/precache-manifest.2bae70c741bc83b684ec0dd410f2657f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/looneytunes/images/e/e5/Gunfade.gif/revision/latest/scale-to-width-down/250/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
